03d00862a07151f77fc70c243f37f9ae267eff4e
yonathanm10 <yonathanm@cocoindonesia.com>
2024-05-16 23:43:00 +0700

    5: UserService implementation and testing UserService Get using mock repo

---
+++ account/service/user_service_test.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/nobitayon/memorization-app/handler/model"
+	"github.com/nobitayon/memorization-app/handler/model/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGet(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		mockUserRepo := &model.User{
+			UID:   uid,
+			Email: "bob@bob.com",
+			Name:  "Bobby Bobson",
+		}
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		us := NewUserService(&USConfig{
+			UserRepository: mockUserRepository,
+		})
+		mockUserRepository.On("FindByID", mock.Anything, uid).Return(mockUserRepo, nil)
+
+		ctx := context.TODO()
+		u, err := us.Get(ctx, uid)
+
+		assert.NoError(t, err)
+		assert.Equal(t, u, mockUserRepo)
+		mockUserRepository.AssertExpectations(t)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		us := NewUserService(&USConfig{
+			UserRepository: mockUserRepository,
+		})
+		mockUserRepository.On("FindByID", mock.Anything, uid).Return(nil, fmt.Errorf("some error down the call chain"))
+
+		ctx := context.TODO()
+		u, err := us.Get(ctx, uid)
+
+		assert.Nil(t, u)
+		assert.Error(t, err)
+		mockUserRepository.AssertExpectations(t)
+	})
+}
